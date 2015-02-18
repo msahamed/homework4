39ad39baf17acd5ac75b9ff89f5f0259577cdc1f
Md. Ahamed;msahamed@memphis.edu <msahamed@login0.localdomain>
2015-02-18 10:45:10 -0600

    Initial commit with return 0 only

---
+++ homework4.cpp
@@ -0,0 +1,19 @@
+ /* 
+ * Sabber Ahamed
+ * Homework -3
+ * Programming tools for Eanginners and scientists
+ * Spring 2015
+ * Github link : https://github.com/msahamed/ProgrammingTools/tree/master/homeWork3
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+//Main program
+
+int main () {
+
+    return 0;
+}
