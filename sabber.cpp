3634c664bd1180e05e4d0a1402cfce07b5cc0fac
msahamed <msahamed@memphis.edu>
2015-02-19 15:56:53 -0600

    created input file reading function and sabber.in

--- sabber.cpp
+++ sabber.cpp
@@ -1,6 +1,6 @@
  /* 
  * Sabber Ahamed
- * Homework -3
+ * Homework -4
  * Programming tools for Eanginners and scientists
  * Spring 2015
  * Github link : https://github.com/msahamed/ProgrammingTools/tree/master/homeWork3
@@ -8,14 +8,21 @@
 
 
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h> 
+#include <fstream>
+
 using namespace std;
 
 void FibonacciNumber(int number);
+int open_input(string inputFileName);
 
 //Main program
 
 int main () {
     
+
     cout << "\n";
     cout << " I was able to compile this code using the HPC at the University of Memphis." 
          << " When I compiled it there, it did not produce any warning message." 
@@ -29,14 +36,42 @@ int main () {
          << " \n ";
     cout << " \n ";
     
-    int length = 20;
+
+    string inputFileName = "sabber.in" ;
+
+    // open the inputfile :
+    int length = open_input(inputFileName);
+
+    // calling FibonacciNumber function to computer the series
     FibonacciNumber(length);
 }
 
 
+ 
+int open_input(string inputFileName){
+   
+   ifstream inputFile;
+   inputFile.open(inputFileName.c_str());
 
-void FibonacciNumber(int number){
 
+   if (inputFile.fail()) {
+      cout << "Error reading the file  " << inputFileName << endl;
+      exit(EXIT_FAILURE);
+   }
+
+    string line;
+    int length;
+    getline(inputFile, line);
+    istringstream number (line);
+    number >> length;
+    
+    return length; 
+}
+
+
+void FibonacciNumber(int N){
+
+    int number = 10 * N;
     int fibonacciArray[number];
     int first = 0;
     int second = 1;
